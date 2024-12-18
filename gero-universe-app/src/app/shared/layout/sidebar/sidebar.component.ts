6aaeac70d7cda3ad30118f1315412805e58edbab
GerardRuica <gerard.ruica@gmail.com>
2024-12-18 22:24:45 +0100

    navbar improvments

--- gero-universe-app/src/app/shared/layout/sidebar/sidebar.component.ts
+++ gero-universe-app/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, Input } from '@angular/core';
 
+/**
+ * Component that implements a simple sidebar
+ */
 @Component({
   selector: 'sidebar',
   imports: [CommonModule],
@@ -8,29 +11,61 @@ import { Component, HostListener, Input } from '@angular/core';
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  /** Background color of the sidebar */
   @Input() sidebarColor: string = '#171717';
+  /** Logo of the sidebar */
   @Input() logo: string = '';
+  /** Background color of the logo of the sidebar */
   @Input() logoBackgroundColor: string = '';
+  /** Title of the sidebar */
   @Input() title: string = '';
 
+  /** Width of the screen */
   public screenWidth: number = window.innerWidth;
+  /** Boolean that indicates if sidebar is opened or closed */
   public opened: boolean = false;
+  /** Boolean that indicates if screen is small or big */
   public isSmallScreen: boolean = false;
 
+  /** Constructor of the component */
   constructor() {
     this.checkScreenSize();
   }
 
+  /**
+   * Listener that listens screen width
+   *
+   * @param {Event} event Web event
+   */
   @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
+  private onResize(event: Event) {
     this.screenWidth = window.innerWidth;
     this.checkScreenSize();
   }
 
-  public checkScreenSize() {
+  /**
+   * Listener that listens if user click outside navbar and not clicks toggle button
+   *
+   * @param {Event} event  Web event
+   */
+  @HostListener('document:click', ['$event'])
+  private onClickOutside(event: MouseEvent) {
+    const sidebarElement = document.querySelector('.sidebar');
+    const toggleButton = document.querySelector('.toggle-button');
+    const clickedInsideSidebar = sidebarElement?.contains(event.target as Node);
+    const clickedOnToggleButton = toggleButton?.contains(event.target as Node);
+
+    if (!clickedInsideSidebar && !clickedOnToggleButton && this.opened) {
+      this.opened = false;
+    }
+  }
+
+  /** Checks screen size */
+  private checkScreenSize() {
     this.isSmallScreen = this.screenWidth < 900;
   }
 
+  /** Change status of the sidebar to open or to closed */
   public toggleSidebar() {
     this.opened = !this.opened;
   }

9c84e26b66ece0d41bccd5b4228927108e9730fd
GerardRuica <gerard.ruica@gmail.com>
2024-12-18 21:54:08 +0100

    sidebar fixes

--- gero-universe-app/src/app/shared/layout/sidebar/sidebar.component.ts
+++ gero-universe-app/src/app/shared/layout/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
@@ -11,4 +12,26 @@ export class SidebarComponent {
   @Input() logo: string = '';
   @Input() logoBackgroundColor: string = '';
   @Input() title: string = '';
+
+  public screenWidth: number = window.innerWidth;
+  public opened: boolean = false;
+  public isSmallScreen: boolean = false;
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    this.checkScreenSize();
+  }
+
+  public checkScreenSize() {
+    this.isSmallScreen = this.screenWidth < 900;
+  }
+
+  public toggleSidebar() {
+    this.opened = !this.opened;
+  }
 }

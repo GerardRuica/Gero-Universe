c646a4f4c9741d70e562dead18f990016032b281
GerardRuica <gerard.ruica@gmail.com>
2025-01-04 17:28:57 +0100

    Form input and functional login

--- gero-universe-app/src/app/features/authentication/pages/login-page/login-page.component.ts
+++ gero-universe-app/src/app/features/authentication/pages/login-page/login-page.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 import { FormInputComponent } from '../../../../shared/inputs/form-input/form-input.component';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../../services/auth-service/auth.service';
+import { UserLogin } from '../../../../types/userTypes';
 
+/**
+ * Page to login an user
+ */
 @Component({
   selector: 'login-page',
-  imports: [FormInputComponent],
+  imports: [FormInputComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  /** Login form */
+  public loginForm: FormGroup;
+
+  /**
+   * Constructor that import all dependencies and initializes form
+   *
+   * @param {FormBuilder} formBuilder Form builder to creates form
+   * @param {AuthService} authService Auth service to login
+   */
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password }: UserLogin = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          console.log('Successful login', response);
+        },
+        error: (error) => {
+          console.error('Error when login ', error);
+        },
+      });
+    }
+  }
+}
--- gero-universe-app/src/app/shared/inputs/form-input/form-input.component.ts
+++ gero-universe-app/src/app/shared/inputs/form-input/form-input.component.ts
@@ -1,13 +1,78 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input, Provider } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
+/** Provider to do form input */
+export const CUSTOM_CONTROL_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => FormInputComponent),
+  multi: true,
+};
+
+/**
+ * This component creates a customizable input field that can be used with reactive forms.
+ * It implements `ControlValueAccessor` to integrate with Angular's form API,
+ * allowing it to be used with `formControlName` in a reactive form.
+ */
 @Component({
   selector: 'form-input',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './form-input.component.html',
   styleUrl: './form-input.component.scss',
+  providers: [CUSTOM_CONTROL_VALUE_ACCESSOR],
 })
-export class FormInputComponent {
+export class FormInputComponent implements ControlValueAccessor {
+  /** Label text of the input */
   @Input() public labelText: string = '';
+  /** Placeholder text of the input */
   @Input() public placeholderText: string = '';
+  /** Input type */
   @Input() public type: string = 'text';
+
+  /** Internal value bound to the input field. */
+  public value: string = '';
+  /** Callback for handling changes in the input value */
+  public onChange = (value: string) => {};
+  /** Callback for handling touch events */
+  public onTouched = () => {};
+
+  public onInputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;
+    this.onChange(this.value);
+  }
+
+  /**
+   * Writes a new value to the input field
+   * @param value The value to write to the input
+   */
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  /**
+   * Registers a function to call when the value changes
+   * @param fn The function to call on value change
+   */
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registers a function to call when the input is touched
+   * @param fn The function to call on touch
+   */
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Sets the disabled state of the input field
+   * @param {boolean} isDisabled Whether the input should be disabled
+   */
+  public setDisabledState?(isDisabled: boolean): void {}
 }

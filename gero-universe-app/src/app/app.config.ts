1c206990aee801612a5eaf1c942b4099cee4ce49
GerardRuica <gerard.ruica@gmail.com>
2025-01-19 13:12:05 +0100

    Resolved traductions bug

--- gero-universe-app/src/app/app.config.ts
+++ gero-universe-app/src/app/app.config.ts
@@ -9,28 +9,29 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
+  http: HttpClient
+) => new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-    importProvidersFrom(
+    provideHttpClient(withFetch()),
+    importProvidersFrom([
       TranslateModule.forRoot({
         loader: {
           provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
+          useFactory: httpLoaderFactory,
           deps: [HttpClient],
         },
-      })
-    ),
+        defaultLanguage: 'es',
+      }),
+    ]),
   ],
 };
--- gero-universe-app/src/app/features/home/home.component.ts
+++ gero-universe-app/src/app/features/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AppCardComponent } from '../../shared/cards/app-card/app-card.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [AppCardComponent],
+  imports: [AppCardComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
